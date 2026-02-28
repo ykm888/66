d032494108c83f8d07be86088c5771232837d2ce
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-03-01 00:37:37 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -58,13 +58,14 @@ define Device/sl_3000-emmc
   DEVICE_DTS := mt7981-sl-3000-emmc
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   SUPPORTED_DEVICES := sl,3000-emmc
+  DEVICE_DRAM_SIZE := 1024M
   DEVICE_PACKAGES := $(MT7981_USB_PKGS) f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
 	luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
   KERNEL_LOADADDR := 0x44000000
   KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
   KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
   KERNEL_INITRAMFS_SUFFIX := -recovery.itb
-  IMAGES := sysupgrade.bin sdcard.img.gz factory.img.gz
+  IMAGES := sysupgrade.bin factory.img.gz
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   ARTIFACTS := emmc-gpt.bin emmc-preloader.bin emmc-bl31-uboot.fip
   ARTIFACT/emmc-gpt.bin := mt798x-gpt emmc
@@ -74,13 +75,5 @@ define Device/sl_3000-emmc
 	pad-to 17k | mt7981-bl2 emmc-ddr3 |\
 	pad-to 6656k | mt7981-bl31-uboot sl_3000-emmc |\
 	pad-to 64M | append-image squashfs-sysupgrade.itb | gzip
-  ARTIFACT/sdcard.img.gz := mt798x-gpt sdmmc |\
-	pad-to 17k | mt7981-bl2 sdmmc-ddr3 |\
-	pad-to 6656k | mt7981-bl31-uboot sl_3000-emmc |\
-	$(if $(CONFIG_TARGET_ROOTFS_INITRAMFS),\
-		pad-to 12M | append-image-stage initramfs-recovery.itb | check-size 44m |\
-	) \
-	pad-to 64M | append-image squashfs-sysupgrade.itb | check-size |\
-	gzip
 endef
 TARGET_DEVICES += sl_3000-emmc
