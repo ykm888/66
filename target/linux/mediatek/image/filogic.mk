1d2975908e75c2a466c6bbc46d0350a5e2c24beb
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-02-28 11:42:28 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -33,29 +33,82 @@ endef
 define Build/mt798x-gpt
 	cp $@ $@.tmp 2>/dev/null || true
 	ptgen -g -o $@.tmp -a 1 -l 1024 \
-		$(if $(findstring spim-nand,$1), -p 512k@256k:env -p 1024k@768k:factory -p 2048k@1792k:fip) \
-		$(if $(findstring emmc,$1), -p 32M@2M:fip)
+		$(if $(findstring sdmmc,$1), \
+			-H \
+			-t 0x83	-N bl2		-r	-p 4079k@17k \
+		) \
+			-t 0x83	-N ubootenv	-r	-p 512k@4M \
+			-t 0x83	-N factory	-r	-p 2M@4608k \
+			-t 0xef	-N fip		-r	-p 4M@6656k \
+				-N recovery	-r	-p 32M@12M \
+		$(if $(findstring sdmmc,$1), \
+				-N install	-r	-p 20M@44M \
+			-t 0x2e -N production		-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@64M \
+		) \
+		$(if $(findstring emmc,$1), \
+			-t 0x2e -N production		-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@64M \
+		)
 	cat $@.tmp >> $@
 	rm $@.tmp
 endef
 
-define Device/abt_sl3000
-  DEVICE_VENDOR := ABT
+metadata_gl_json = \
+	'{ $(if $(IMAGE_METADATA),$(IMAGE_METADATA)$(comma)) \
+		"metadata_version": "1.1", \
+		"compat_version": "$(call json_quote,$(compat_version))", \
+		$(if $(DEVICE_COMPAT_MESSAGE),"compat_message": "$(call json_quote,$(DEVICE_COMPAT_MESSAGE))"$(comma)) \
+		$(if $(filter-out 1.0,$(compat_version)),"new_supported_devices": \
+			[$(call metadata_devices,$(SUPPORTED_DEVICES))]$(comma) \
+			"supported_devices": ["$(call json_quote,$(legacy_supported_message))"]$(comma)) \
+		$(if $(filter 1.0,$(compat_version)),"supported_devices":[$(call metadata_devices,$(SUPPORTED_DEVICES))]$(comma)) \
+		"version": { \
+			"release": "$(call json_quote,$(VERSION_NUMBER))", \
+			"date": "$(shell TZ='Asia/Chongqing' date '+%Y%m%d%H%M%S')", \
+			"dist": "$(call json_quote,$(VERSION_DIST))", \
+			"version": "$(call json_quote,$(VERSION_NUMBER))", \
+			"revision": "$(call json_quote,$(REVISION))", \
+			"target": "$(call json_quote,$(TARGETID))", \
+			"board": "$(call json_quote,$(if $(BOARD_NAME),$(BOARD_NAME),$(DEVICE_NAME)))" \
+		} \
+	}'
+
+define Build/append-gl-metadata
+	$(if $(SUPPORTED_DEVICES),-echo $(call metadata_gl_json,$(SUPPORTED_DEVICES)) | fwtool -I - $@)
+	sha256sum "$@" | cut -d" " -f1 > "$@.sha256sum"
+	[ ! -s "$(BUILD_KEY)" -o ! -s "$(BUILD_KEY).ucert" -o ! -s "$@" ] || { \
+		cp "$(BUILD_KEY).ucert" "$@.ucert" ;\
+		usign -S -m "$@" -s "$(BUILD_KEY)" -x "$@.sig" ;\
+		ucert -A -c "$@.ucert" -x "$@.sig" ;\
+		fwtool -S "$@.ucert" "$@" ;\
+	}
+endef
+
+define Device/weihua_sl3000
+  DEVICE_VENDOR := WEIHUA
   DEVICE_MODEL := SL3000
   DEVICE_VARIANT := (eMMC)
-  DEVICE_DTS := mt7981b-3000-emmc
+  DEVICE_DTS := mt7981b-weihua-sl3000
   DEVICE_DTS_DIR := ../dts
-  SUPPORTED_DEVICES := abt,sl3000
-  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3 kmod-mt7915e
-  KERNEL := kernel-bin | lzma
-  KERNEL_INITRAMFS := kernel-bin | lzma | \
-        fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  IMAGE_SIZE := 102400k
-  IMAGES := sysupgrade.itb
-  IMAGE/sysupgrade.itb := append-kernel | append-rootfs | \
-        monitor-fip mt7981-emmc-abt_sl3000 | pad-to 128k | append-metadata
-  ARTIFACTS := preloader.bin u-boot.fip
-  ARTIFACT/preloader.bin := mt7981-bl2 emmc-ddr3
-  ARTIFACT/u-boot.fip := mt7981-bl31-uboot abt_sl3000
+  DEVICE_DTS_LOADADDR := 0x47000000
+  DEVICE_PACKAGES := kmod-usb3 kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware \
+	e2fsprogs f2fsck mkf2fs automount
+  KERNEL_LOADADDR := 0x44000000
+  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  IMAGES := sysupgrade.itb sdcard.img.gz
+  IMAGE/sysupgrade.itb := append-kernel | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-with-rootfs | pad-rootfs | append-metadata
+  ARTIFACTS := emmc-gpt.bin emmc-preloader.bin emmc-bl31-uboot.fip
+  ARTIFACT/emmc-gpt.bin := mt798x-gpt emmc
+  ARTIFACT/emmc-preloader.bin := mt7981-bl2 emmc-ddr3
+  ARTIFACT/emmc-bl31-uboot.fip := mt7981-bl31-uboot weihua_sl3000
+  ARTIFACT/sdcard.img.gz := mt798x-gpt sdmmc |\
+	pad-to 17k | mt7981-bl2 sdmmc-ddr3 |\
+	pad-to 6656k | mt7981-bl31-uboot weihua_sl3000 |\
+	$(if $(CONFIG_TARGET_ROOTFS_INITRAMFS),\
+		pad-to 12M | append-image-stage initramfs-recovery.itb | check-size 44m |\
+	) \
+	pad-to 64M | append-image squashfs-sysupgrade.itb | check-size |\
+	gzip
 endef
-TARGET_DEVICES += abt_sl3000
+TARGET_DEVICES += weihua_sl3000
