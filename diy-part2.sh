444fa82a58af3f6b4c21099a3f75ad89b18db316
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-03-09 15:39:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# 物理操作：修改默认配置
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 修改默认后台 IP (建议改成你习惯的，比如 192.168.1.1)
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. 修改主机名为 SL3000 (让设备看起来更专业)
+sed -i 's/OpenWrt/SL3000-Router/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. 物理修正：某些情况下 eMMC 引导需要调整分区挂载逻辑 (可选，针对 23.05)
+# sed -i 's/rootfs_data/userdata/g' target/linux/mediatek/image/filogic.mk
+
+# 4. 修改默认主题为 Argon (如果你的 config 里选了这个主题)
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
