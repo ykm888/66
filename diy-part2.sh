51372b88742d98a38f245f95318add2eb848d8ca
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-03-10 14:54:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,10 +9,9 @@ cp -f ../custom-config/mt7981-sl-3000-emmc.dts target/linux/mediatek/dts/
 cp -f ../custom-config/filogic.mk target/linux/mediatek/image/
 
 # 最小物理修补：对齐日志报错路径 (Linux 6.12.74)
-# 禁止添加 if/else 探测逻辑
 mkdir -p target/linux/mediatek/files-6.12/arch/arm64/boot/dts/mediatek/
 cp -f ../custom-config/mt7981-sl-3000-emmc.dts target/linux/mediatek/files-6.12/arch/arm64/boot/dts/mediatek/
 
-# 物理复刻：清理原有uboot零件路径 (根据你的工序结构)
+# 物理复刻：清理原有uboot零件路径
 rm -rf package/boot/uboot-mediatek
 cp -rf ../custom-config/uboot-mediatek package/boot/
