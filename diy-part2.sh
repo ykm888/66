7171971ae3c2d29029e3b5a23ea55fe1e24f83d3
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-03-12 21:22:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,18 +5,17 @@ if [ -d "feeds/luci/applications/luci-app-easymesh" ]; then
     find feeds/luci/applications/luci-app-easymesh -name "Makefile" | xargs sed -i 's/+wpa-supplicant-mesh-openssl//g' || true
 fi
 
-# 512MB 物理锁定
+# 512MB 物理锁定与默认 IP 修正
 sed -i '/CONFIG_ARM64_VA_BITS_39/d' .config
 sed -i '/CONFIG_ARM64_PA_BITS_40/d' .config
 sed -i '/CONFIG_PGTABLE_LEVELS/d' .config
 echo "CONFIG_ZONE_DMA32=y" >> .config
 
-# 修正 IP
 [ -f "package/base-files/files/bin/config_generate" ] && {
     sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate || true
 }
 
-# 屏蔽 Werror 警告导致的中断
+# 暴力抹除 Werror 警告导致的中断退出
 find package/boot feeds/mediatek -name "Makefile" 2>/dev/null | xargs sed -i 's/-Werror//g' || true
 find package/boot feeds/mediatek -name "Makefile" 2>/dev/null | xargs sed -i 's/-no-warn-rwx-segments//g' || true
 
