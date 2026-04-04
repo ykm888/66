bf9f3c5672206baa8b408b68c18fdf1f12a90d68
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-04-04 12:19:43 +0800

    Update mt7981_sl3000.mk

--- 888/mt7981_sl3000.mk
+++ 888/mt7981_sl3000.mk
@@ -1,14 +1,22 @@
-define Device/sl3000-emmc
+define Device/mt7981_sl3000_emmc
   DEVICE_VENDOR := SL
   DEVICE_MODEL := SL3000
   DEVICE_VARIANT := eMMC
+  DEVICE_DTS := mt7981b-sl3000-emmc
+  SUPPORTED_DEVICES := sl,sl3000
+
+  # 🔥 救砖全家桶（核心全部在这里）
   DEVICE_PACKAGES := \
 	luci luci-base luci-mod-system luci-theme-bootstrap \
-	f2fs-tools e2fsprogs \
-	blockd mount-utils
-  DEVICE_DTS := mt7981b-sl3000-emmc
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := sl,sl3000
+	fail2ban \
+	block-mount blockd e2fsprogs f2fs-tools parted losetup kmod-fs-ext4 kmod-fs-f2fs \
+	dropbear openssh-sftp-server \
+	usbutils lsblk blkid mount-utils swap-utils \
+	kmod-usb-storage kmod-usb-storage-uas kmod-usb3 \
+	htop iftop iperf3 net-tools tcpdump wget-nossl \
+	adminip ip-full ip-bridge kmod-bridge \
+	uboot-envtools uboot-envtools-mtk
+
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += sl3000-emmc
+TARGET_DEVICES += mt7981_sl3000_emmc
