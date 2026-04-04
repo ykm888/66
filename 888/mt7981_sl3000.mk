0e862633ad4408867a7d22a5ae1ebe0d9134da3d
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-04-04 10:11:24 +0800

    Update mt7981_sl3000.mk

--- 888/mt7981_sl3000.mk
+++ 888/mt7981_sl3000.mk
@@ -1,12 +1,16 @@
-define Device/sl_3000-emmc
+define Device/sl_3000-spi-nor
   DEVICE_VENDOR := SL
-  DEVICE_MODEL := 3000 eMMC (512MB rescue)
+  DEVICE_MODEL := 3000 SPI-NOR (32MB)
   DEVICE_DTS := mt7981-sl-3000-emmc
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := sl,3000-emmc
-  DEVICE_PACKAGES := $(MT7981_USB_PKGS) f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
-	luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
+  DEVICE_DTS_DIR := $(DTS_DIR)
+  SUPPORTED_DEVICES := sl,3000-spi-nor
+  DEVICE_PACKAGES := \
+    block-mount \
+    uboot-envtools \
+    ttyd
   IMAGES := sysupgrade.bin
+  IMAGE_SIZE := 25600k
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += sl_3000-emmc
+
+TARGET_DEVICES += sl_3000-spi-nor
