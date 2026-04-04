facf04ec378c8ae4357d4eb2496434a8035e22a2
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-04-04 20:22:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,10 +2,14 @@
 set -euo pipefail
 
 WORKSPACE="$GITHUB_WORKSPACE"
-OUTPUT_DIR="$WORKSPACE/output"
+export WORKSPACE
 
+OUTPUT_DIR="$WORKSPACE/output"
 mkdir -p $OUTPUT_DIR/atf
 mkdir -p $OUTPUT_DIR/uboot
 
+# 只装救砖编译依赖
 sudo apt update
-sudo apt install -y build-essential gcc-aarch64-linux-gnu make libssl-dev device-tree-compiler bc
+sudo apt install -y \
+  build-essential gcc-aarch64-linux-gnu \
+  make libssl-dev device-tree-compiler bc
