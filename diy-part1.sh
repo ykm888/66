68a9e59c3bd8d1ee81c728b9d3389425e64183ee
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-04-04 20:21:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,10 +6,6 @@ OUTPUT_DIR="$WORKSPACE/output"
 
 mkdir -p $OUTPUT_DIR/atf
 mkdir -p $OUTPUT_DIR/uboot
-mkdir -p $OUTPUT_DIR/mtk_uartboot
 
-# 只装救砖编译依赖
 sudo apt update
 sudo apt install -y build-essential gcc-aarch64-linux-gnu make libssl-dev device-tree-compiler bc
-
-echo "✅ 环境准备完成"
