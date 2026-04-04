e65173dfe60b3200a3311a41f5b585d7fa2cfe87
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-04-04 12:31:32 +0800

    Update mt7981_sl3000.mk

--- 888/mt7981_sl3000.mk
+++ 888/mt7981_sl3000.mk
@@ -1,26 +1,19 @@
 define Device/mt7981_sl3000_emmc
   DEVICE_VENDOR := SL
   DEVICE_MODEL := SL3000
-  DEVICE_VARIANT := NOR-32MB
+  DEVICE_VARIANT := eMMC
   DEVICE_DTS := mt7981b-sl3000-emmc
   SUPPORTED_DEVICES := sl,sl3000
 
-  # ==============================
-  # 32MB SPI NOR (BY25Q256FS) 标准分区
-  # ==============================
-  KERNEL_SIZE := 4096k
-  ROOTFS_SIZE := 26624k
-  IMAGE_SIZE := 30720k
-  BLOCKSIZE := 64k
   KERNEL_LOADADDR := 0x48000000
 
-  # 救砖全家桶（精简不超容）
+  # eMMC 救砖完整包（适配你机器）
   DEVICE_PACKAGES := \
 	luci luci-base luci-mod-system luci-theme-bootstrap \
 	block-mount e2fsprogs f2fs-tools \
 	kmod-fs-ext4 kmod-fs-f2fs \
 	kmod-mtd kmod-mtd-rw \
-	kmod-spi-nor kmod-sdhci-mtk \
+	kmod-sdhci-mtk \
 	dropbear \
 	lsblk blkid mount-utils \
 	mtd-utils uboot-envtools
