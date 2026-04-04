8618ce6adce805f0bd175471d97b9686757841a6
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-04-04 12:10:42 +0800

    Update mt7981_sl3000.mk

--- 888/mt7981_sl3000.mk
+++ 888/mt7981_sl3000.mk
@@ -1,25 +1,14 @@
-define Device/sl_3000-spi-nor
+define Device/sl3000-emmc
   DEVICE_VENDOR := SL
-  DEVICE_MODEL := 3000 SPI-NOR (32MB Flash)
-  DEVICE_DTS := mt7981-sl-3000-emmc
-  DEVICE_DTS_DIR := $(DTS_DIR)
-  SUPPORTED_DEVICES := sl,3000-spi-nor
-  # 救砖全家桶（含你之前要的 spi、mmc、分区、uboot工具）
+  DEVICE_MODEL := SL3000
+  DEVICE_VARIANT := eMMC
   DEVICE_PACKAGES := \
-    block-mount \
-    uboot-envtools \
-    ttyd \
-    mmc-utils \
-    spi-utils \
-    mtd-utils \
-    kmod-mtd-rw
-
-  # 关键：只生成完整编程器固件 .bin，不生成 sysupgrade
-  IMAGES := factory.bin
-  IMAGE_SIZE := 32768k
-
-  # 完整一体固件（直接用于编程器烧录 SPI NOR 32MB）
-  IMAGE/factory.bin := append-kernel | append-rootfs | pad-rootfs | pad-to 32768k
+	luci luci-base luci-mod-system luci-theme-bootstrap \
+	f2fs-tools e2fsprogs \
+	blockd mount-utils
+  DEVICE_DTS := mt7981b-sl3000-emmc
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := sl,sl3000
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-
-TARGET_DEVICES += sl_3000-spi-nor
+TARGET_DEVICES += sl3000-emmc
